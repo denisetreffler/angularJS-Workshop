982069a69b0e9ceda0783346b92a3cad2b0f3a98
Marc <marc.baur@swisscom.com>
2015-10-15 18:35:28 +0200

    Adjusted go.sh after testing on Swisscom VM

--- go.sh
+++ go.sh
@@ -5,32 +5,17 @@ set -e
 echo "Configure git to use https instead of ssh for github URLs"
 git config --global url."https://github".insteadOf git://github
 
-echo "Uninstalling old ruby1.8 installation"
-sudo apt-get update
-sudo apt-get remove ruby1.8
-
-echo "Installing ruby 1.9.1 for the sass gem to work"
-sudo apt-get update
-sudo apt-get install ruby1.9.1 ruby1.9.1-dev \
-  rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
-  build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev
-sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
-         --slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz \
-                        /usr/share/man/man1/ruby1.9.1.1.gz \
-        --slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
-        --slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
-        --slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
-
-echo "Installing bower"
+echo "Installing bower globally"
 sudo npm install bower -g
 
-echo "Installing sass (this will take while)"
+echo "Installing sass)"
 sudo gem install sass
 
 echo "Running npm install'"
 npm install
 
 echo "Running bower install'"
+export http_proxy = http://proxy.corproot.net:8079/
 bower install
 
 echo "Running grunt build'"
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Vorbereitung vor Workshop
 1. Projekt in IntelliJ auschecken
-2. Shell Script für Initialisierung starten mittels `./go.sh` im Hauptverzeichnis
+2. Shell Script für Initialisierung starten mittels `sh go.sh` im Hauptverzeichnis
 5. Node Applikation starten mittels `node server.js`
 4. Im Browser auf `http://localhost:3000` gehen und prüfen ob der Titel "Das war's schon ;-)" (inkl. Smiley) angezeigt wird. Falls ja, war's das. Ansonsten kannst du dich gerne bei uns melden.
