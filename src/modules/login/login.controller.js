d916461556a7374c1ecc50b965d62a7df64d2c87
denise <denise.treffler@ec4u.de>
2016-06-17 23:03:55 +0200

    login model

--- src/modules/login/login.controller.js
+++ src/modules/login/login.controller.js
@@ -4,9 +4,5 @@ var loginModule = angular.module("modules.login", []);
 
 loginModule.controller("loginController",
     function($scope) {
-        $scope.user = {
-            id: 1,
-            name: 'George Clooney',
-            pw: 'batman'
-        };
+        // tbd
     });
