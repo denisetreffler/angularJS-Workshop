2051f3e090a1f38ddfa1f4d7a96ae6aef3ac6782
denise <denise.treffler@ec4u.de>
2016-06-24 23:14:28 +0200

    logout logic

--- src/modules/login/login.controller.js
+++ src/modules/login/login.controller.js
@@ -9,9 +9,14 @@ loginModule.controller("loginController",
 
         vm.loggedIn = true;
         vm.loginData = LoginService.getUser();
+        vm.logout = logout;
     }
 );
 
 loginModule.factory("LoginService",
     LoginService
 );
+
+function logout() {
+    this.loggedIn = false;
+};
