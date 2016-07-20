9e807ec5724a674cfbcc8bd8720c99751003e057
denise <denise.treffler@ec4u.de>
2016-07-20 23:12:08 +0200

    [practice5] ng-click

--- src/app.js
+++ src/app.js
@@ -8,4 +8,9 @@ angular.module('ec4uWorkshop', [
         ['$scope', function($scope){
         $scope.welcomeTitle = "Das war's schon";
         $scope.welcomeMessage = "Bis im Workshop, wir freuen uns!";
-    }]);
+        }])
+    .controller('clickController', function($scope){
+        $scope.value = 0;
+        $scope.counter = function(){
+            $scope.value += 1;
+        }});
