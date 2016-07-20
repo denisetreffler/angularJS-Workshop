a13cf8ed49c8f8f5a6331940699821d622031070
denise <denise.treffler@ec4u.de>
2016-07-20 17:05:44 +0200

    [practice1] added personcontroller

--- src/app.js
+++ src/app.js
@@ -7,4 +7,9 @@ angular.module('ec4uWorkshop', [
         ['$scope', function($scope){
             $scope.welcomeTitle = "Das war's schon";
             $scope.welcomeMessage = "Bis im Workshop, wir freuen uns!";
-        }]);
+        }])
+    .controller('PersonController', function ($scope) {
+        $scope.person = {
+            lastname: "Treffler",
+            firstname: "Denise"
+        }});
