c08367965fb4bc5c74fa4a3a576f72d8c5dccbdf
denise <denise.treffler@ec4u.de>
2016-07-20 19:10:46 +0200

    [practice2] different controllers and scoping

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ angular.module('ec4uWorkshop', [
         ['$scope', function($scope){
         $scope.welcomeTitle = "Das war's schon";
         $scope.welcomeMessage = "Bis im Workshop, wir freuen uns!";
-    }]);
+        }])
+    .controller('aController',
+        ['$scope', function($scopesssss){
+            $scopesssss.name1 = "aController value1";
+            $scopesssss.name2 = "aController value2";
+        }])
+    .controller('bController', function ($scope) {
+        $scope.obj = {
+            name1: "bController value1",
+            name2: "bController value2"
+        }})
+    .controller('cController', function () {
+        this.obj = {
+            name1: "cController value1",
+            name2: "cController value2"
+        }})
+    .controller('dController', function ($scope) {
+        $scope.obj = {
+            name1: "dController value1",
+            name2: "dController value2"
+        }});
